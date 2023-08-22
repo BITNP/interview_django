6a703ea1ac11e963f238336499a26d73e214b777
everything411 <everything411@163.com>
2023-08-22 16:40:16 +0800

    run nginx in background

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
+/usr/sbin/nginx
 python3 manage.py migrate
 gunicorn -w 4 --forwarded-allow-ips='*' -b unix:/tmp/gunicorn.sock -k uvicorn.workers.UvicornWorker interview_django.asgi:application
